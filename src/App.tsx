bef747ff3b1f20119bdaffbebde300fdc4e30f08
TJ Epperson <tj.epperson070@gmail.com>
2022-05-07 23:14:59 +0900

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from 'react-redux'
+import './App.css'
+import Login from './components/Login'
+import Matchmaking from './components/Matchmaking'
+import Game from './components/Game'
+import { AccountState } from './store/account'
+import { GameState } from './store/game'
 
 function App() {
+  const isLoggedIn = useSelector((state: AccountState) => state.account.isLoggedIn)
+  const isPlaying = useSelector((state: GameState) => state.game.isPlaying)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isLoggedIn ? <Login /> : 
+       !isPlaying ? <Matchmaking /> : <Game />}
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
